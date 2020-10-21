cb7d993b881fe2b1739eb1529975015bb93beff0
melashry <mohamedelashry323@gmail.com>
2020-10-21 13:31:09 +0200

    el3shry

--- app/src/main/java/com/gizanfish/tags/Tags.java
+++ app/src/main/java/com/gizanfish/tags/Tags.java
@@ -7,7 +7,7 @@ public class Tags {
     //aass@123456
     //https://documenter.getpostman.com/view/12895224/TVYAhgaB
     //https://xd.adobe.com/view/a66e244a-3927-4a9b-a5ea-62ae74fc0178-f33d/screen/9a7dda83-d29f-420b-8fac-0619193d041c/
-    public static String base_url = "http://fish.technologycircles.com.sa/";
+    public static String base_url = "http://jizanfish.com/";
     public static final String IMAGE_URL = base_url + "storage/";
     public static final String session_login = "login";
     public static final String session_logout = "logout";
